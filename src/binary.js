2ccc40bb829a0ddc643f935d1ed9c9e664060f13
Chris-Pagan <christopher.pagan@outlook.com>
2022-01-04 17:46:43 -0800

    fixed small bug

--- src/binary.js
+++ src/binary.js
@@ -1,3 +1,17 @@
-const binary = (max) => {};
+const Queue = require("./lib/queue")
+
+const binary = (max) => {
+    const nums = new Queue();
+    nums.enqueue("1");
+    let result = []
+
+    for (let i = 1; i <= max; i++){
+        let newNum = nums.dequeue();
+        result.push(newNum)
+        nums.enqueue(newNum.concat("0"));
+        nums.enqueue(newNum.concat("1"));
+    }
+    return result;
+};
 
 module.exports = binary;
--- src/brackets.js
+++ src/brackets.js
@@ -1,5 +1,32 @@
 const Stack = require("./lib/stack");
 
-const match = (expression) => {};
+const match = (expression) => {
+    const newStack = new Stack();
+  
+    for (let i = 0; i < expression.length; i++) {
+      switch (expression[i]) {
+        case "(":
+        case "[":
+        case "{":
+          newStack.push(expression[i]);
+          break;
+        case ")":
+          if (newStack.top?.value === "(") {
+            newStack.pop();
+          } else return false;
+          break;
+        case "]":
+          if (newStack.top?.value === "[") {
+            newStack.pop();
+          } else return false;
+          break;
+        case "}":
+          if (newStack.top?.value === "{") {
+            newStack.pop();
+          } else return false;
+      }
+    }
+    return !newStack.top;
+  };
 
 module.exports = match;
